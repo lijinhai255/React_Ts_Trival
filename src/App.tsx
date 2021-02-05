26f9c8594fbee565093da9e5421880b06089b9e4
liinhai <18854109500@163.com>
2021-02-05 16:20:24 +0800

    【主页开发】走马灯与侧边栏多重菜单

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import styles from "./App.module.css";
-import {Input, Layout,Typography,Menu,Dropdown,Button} from "antd"
+import {Input, Layout,Typography,Menu,Dropdown,Button,Row,Col} from "antd"
 import { GlobalOutlined } from "@ant-design/icons";
-import {Header,Footer} from "./components"
+import {Header,Footer,SideMenu,Carousel} from "./components"
 function App() {
   return (
     <div className={styles.App}>
-     <Header></Header>
-    <Footer></Footer>
+    <Header />
+    {/* 页面内容 content */}
+    <div className={styles["page-content"]}>
+      <Row style={{ marginTop: 20 }}>
+        <Col span={6}>
+          <SideMenu />
+        </Col>
+        <Col span={18}>
+          <Carousel />
+        </Col>
+      </Row>
     </div>
+    <Footer />
+  </div>
   );
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 
 export * from './footer'
 export * from './header'
+export * from './carousel'
+export * from './sideMenu'
